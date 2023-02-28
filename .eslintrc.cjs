356593b605e5521495e796ee8be78fc78995824a
HenryC-3 <2324375439@qq.com>
2023-02-28 18:05:45 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:vue/vue3-essential",
+		"plugin:@typescript-eslint/recommended",
+		"google",
+		"prettier",
+	],
+	overrides: [],
+	parser: "vue-eslint-parser",
+	parserOptions: {
+		parser: "@typescript-eslint/parser",
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["vue", "@typescript-eslint"],
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
+};
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./src/manifest.config";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [vue(), crx({ manifest })],
+});
