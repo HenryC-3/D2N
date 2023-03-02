cfbae593e03dd6bc0381ba00c97497c98687e9c0
HenryC-3 <2324375439@qq.com>
2023-03-02 13:38:42 +0800

    WIP(content-script): content script now grabs book information and send it to background script

--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,63 @@
-alert("this is content script");
+import { Book } from "./types";
+import * as cheerio from "cheerio";
+import type { Cheerio } from "cheerio";
+import { blobToDataURL } from "blob-util";
+
+getBookInfo()
+	.then((book) => {
+		chrome.runtime.sendMessage({ book });
+	})
+	.catch((error) => {
+		console.log(error);
+	});
+
+async function getBookInfo() {
+	const page = document.documentElement.outerHTML;
+	const $ = cheerio.load(page);
+	const coverBlobStr = await getBookCoverBlobStr();
+	const book: Book = {
+		title: $("h1 span").text(),
+		author: $("#info span:contains('作者')").next().text(),
+		publisher: $("#info span:contains('出版社')").next().text(),
+		producer: $("#info span:contains('出品方')").next().text(),
+		subtitle: getInfoByAnchor($("#info span:contains('副标题')")),
+		publishDate: getInfoByAnchor($("#info span:contains('出版年')")),
+		pageCount: parseInt(getInfoByAnchor($("#info span:contains('页数')"))),
+		price: parseFloat(
+			getInfoByAnchor($("#info span:contains('定价')")).replace("元", "")
+		),
+		ISBN: parseInt(getInfoByAnchor($("#info span:contains('ISBN')"))),
+		rating: parseFloat($(".rating_num").text().trim()),
+		ratingCount: parseInt($(".rating_sum a").text().trim()),
+		cover: coverBlobStr,
+	};
+
+	return book;
+
+	// 	find and format target information of the book
+	function getInfoByAnchor(anchor: Cheerio<cheerio.Element>) {
+		const source = anchor[0].nextSibling as unknown as Element;
+		if (source.nodeValue) {
+			return source.nodeValue.trim();
+		}
+		return "";
+	}
+
+	async function getBookCoverBlobStr() {
+		const coverURL = $("#mainpic a.nbg").attr("href");
+		if (coverURL) {
+			const res = await fetch(coverURL);
+			const blob = await res.blob();
+			const blobStr = await blobToDataURL(blob);
+
+			// see if I really get the image form the server
+			// const imageUrl = URL.createObjectURL(blob);
+			// const img = document.createElement("img");
+			// img.src = imageUrl;
+			// document.body.appendChild(img);
+
+			return blobStr;
+		}
+		return "";
+	}
+}
---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export interface Book {
+	title: string;
+	author: string;
+	publisher: string;
+	producer: string;
+	subtitle: string;
+	publishDate: string;
+	pageCount: number;
+	price: number;
+	ISBN: number;
+	rating: number; // 豆瓣星
+	ratingCount: number; // 评价人数
+	cover: string;
+}
