53a48bf42e5e5b36829f79c387953e286ea50889
HenryC-3 <2324375439@qq.com>
2023-03-06 13:48:57 +0800

    ehancement: should provide a action for every key in OneTimeMessage interface

--- src/background/types.ts
+++ src/background/types.ts
@@ -7,7 +7,7 @@ export interface BackgroundStore {
 
 export type ActionForOneTimeMessages = {
 	// eslint-disable-next-line no-unused-vars
-	[k in keyof OneTimeMessage]: Action;
+	[k in keyof Required<OneTimeMessage>]: Action;
 };
 export type Action = (
 	messageValue: OneTimeMessage[keyof OneTimeMessage],
