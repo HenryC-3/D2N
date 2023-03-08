aa09f72d2b125f3205e899ab1f780b1ee8679532
HenryC-3 <2324375439@qq.com>
2023-03-08 17:46:32 +0800

    feature: support modifiy the secret and database ID

--- src/popup/components/AuthPage.vue
+++ src/popup/components/AuthPage.vue
@@ -1,11 +1,27 @@
 <template>
 	<div class="flex flex-col gap-3 p-3 justify-center">
-		<div class="flex justify-center">
-			<h1 v-if="!errName" class="font-semibold text-lg">
+		<div class="flex justify-center font-semibold text-lg">
+			<!-- show database title -->
+			<h1 v-if="databaseTitle" class="text-green-500">
+				Books are stored in:
+				<a
+					@click="
+						() => {
+							openURLInNewTab(databaseURL);
+						}
+					"
+					:href="databaseURL"
+					class="text-[#4EAADC]"
+					>{{ databaseTitle }}</a
+				>
+			</h1>
+			<!-- show default title -->
+			<h1 v-else-if="!errName">
 				<span class="text-green-500">D</span
 				><span class="text-[#4EAADC]">2</span><span>N </span>
 				<span class="text-[#4EAADC]">ꔛꕤ</span>
 			</h1>
+			<!-- show error name -->
 			<h1 v-else class="text-red-400 font-semibold text-lg">
 				{{ errName }}
 			</h1>
@@ -29,7 +45,7 @@
 			class="rounded-sm bg-[#F7F7F5] text-sm border-2 p-2 focus:border-blue-300 focus:outline-none"
 		/>
 		<LoadingButton
-			button-text="Save"
+			:button-text="databaseTitle ? 'Update' : 'Save'"
 			:loading="loadingStatus"
 			shortcut-text="Ctrl + Enter"
 			:shortcut-visibility="true"
@@ -39,21 +55,44 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import LoadingButton from "./LoadingButton.vue";
 import { useRouter } from "vue-router";
 import { sendToBackground } from "../messages";
+import { IndexDB } from "../../background/types";
+import { openURLInNewTab } from "../utils";
 
 const databaseID = ref<string>("");
 const tokenSecret = ref<string>("");
 const router = useRouter();
 
 // UI state
-const loadingStatus = ref(false);
-const errName = ref("");
-const isErr = ref(false);
 const tokenSecretInput = ref<HTMLInputElement | null>();
 const databaseIDInput = ref<HTMLInputElement | null>();
+// UI: button
+const loadingStatus = ref(false);
+// UI: error
+const errName = ref("");
+const isErr = ref(false);
+// UI: configure
+const databaseTitle = ref("");
+const databaseURL = ref("");
+
+// get DB title, id, url and secret from indexDB
+onMounted(() => {
+	sendToBackground(
+		{ getAuthInfo: true },
+		{
+			successAction: (res) => {
+				const response = res as unknown as IndexDB;
+				databaseID.value = response.databaseID;
+				tokenSecret.value = response.tokenSecret;
+				databaseTitle.value = response.databaseTitle;
+				databaseURL.value = response.databaseURL;
+			},
+		}
+	);
+});
 
 const handleClick = () => {
 	// loading
