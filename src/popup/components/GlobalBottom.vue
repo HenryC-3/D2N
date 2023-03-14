ab4472123f868065d6d2b2d2323f43402a0a0251
HenryC-3 <2324375439@qq.com>
2023-03-14 15:49:33 +0800

    ehance: add link

--- src/popup/components/GlobalBottom.vue
+++ src/popup/components/GlobalBottom.vue
@@ -14,8 +14,11 @@
 					d="M11 18h2v-2h-2v2m1-16A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m0-14a4 4 0 0 0-4 4h2a2 2 0 0 1 2-2a2 2 0 0 1 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5a4 4 0 0 0-4-4Z"
 				/>
 			</svg>
-			<!-- TODO: add link to the article -->
-			<a href="" class="text-[12px] text-[#B5B4B3]">Learn More</a>
+			<a
+				href="https://github.com/HenryC-3/D2N.git"
+				class="text-[12px] text-[#B5B4B3]"
+				>Learn More</a
+			>
 		</button>
 		<!-- Navigation button -->
 		<button
