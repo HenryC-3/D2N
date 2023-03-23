b3aa7fa70cc9031a80b6683c37402889a745127f
HenryC-3 <2324375439@qq.com>
2023-03-23 17:08:00 +0800

    build: integrate with semantic-release

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,8 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./src/manifest.config";
-import zipPack from "vite-plugin-zip-pack";
-import packageJson from "./package.json";
-const { version } = packageJson;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		crx({ manifest }),
-		zipPack({ outDir: "./dist-zip", outFileName: `d2n-v${version}.zip` }),
-	],
+	plugins: [vue(), crx({ manifest })],
 });
